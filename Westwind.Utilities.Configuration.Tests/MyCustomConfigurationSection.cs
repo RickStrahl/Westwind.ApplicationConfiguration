91cdb4bc260a2cff66ea286786775139f9b7141e
Rick Strahl <rstrahl@west-wind.com>
2012-12-27 15:46:27 -1000

    Added miscellanouse Configuration tests

---
+++ Westwind.Utilities.Configuration.Tests/MyCustomConfigurationSection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Westwind.Utilities.Configuration
+{
+class MyCustomConfigurationSection : ConfigurationSection
+{        
+    [ConfigurationProperty("ApplicationName")]
+    public string ApplicationName
+    {
+        get { return (string) this["ApplicationName"]; }
+        set { this["ApplicationName"] = value; 
+        
+        
+        }
+    }
+
+    [ConfigurationProperty("MaxDisplayItems",DefaultValue=15)]
+    public int MaxDisplayItems
+    {
+        get { return (int) this["MaxDisplayItems"]; }
+        set { this["MaxDisplayItems"] = value; }
+    }
+
+    [ConfigurationProperty("DebugMode")]
+    public DebugModes DebugMode
+    {
+        get { return (DebugModes) this["DebugMode"]; }
+        set { this["DebugMode"] = value; }
+    }                
+}
+}
---
+++ Westwind.Utilities.Configuration.Tests/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace Westwind.Utilities.Configuration.Tests
+{
+    [TestClass]
+    public class MiscellaneousConfigurationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var settings = ConfigurationManager.GetSection("CustomConfiguration") as NameValueCollection;            
+            Console.WriteLine(settings["ApplicationName"]);
+
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            //object customconfig = config.GetSection("CustomConfiguration") as 
+            //Console.WriteLine(customconfig.ToString());
+            //customconfig.Add("NewKey", "Value");
+            //config.Save();
+        }
+
+        [TestMethod]
+        public void WriteAppSettingsTest()
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var section = config.GetSection("appSettings") as AppSettingsSection;            
+            section.Settings.Add("NewKey", "Value");
+            config.Save();
+        }
+
+        [TestMethod]
+        public void ReadStronglyTypedSectionTest()
+        {
+            var section = ConfigurationManager.GetSection("MyCustomConfiguration") as MyCustomConfigurationSection;
+
+            Console.WriteLine(section.ApplicationName);
+            Console.WriteLine(section.MaxDisplayItems);
+            Console.WriteLine(section.DebugMode);
+
+        }
+    }
+}
